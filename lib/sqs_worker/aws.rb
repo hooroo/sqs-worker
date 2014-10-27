ae42fd0ea2c400397e297f9e8795bb546ff6b5e2
Michael Chapman <michaelc@hooroo.com>
2014-10-27 14:52:37 +1100

    Added spec for AWS class

--- lib/sqs_worker/aws.rb
+++ lib/sqs_worker/aws.rb
@@ -2,13 +2,9 @@ require 'aws-sdk'
 require 'singleton'
 
 module SqsWorker
-  class AWS
+  class Aws
 
     include Singleton
-
-    def initialize
-      @sqs = ::AWS::SQS.new(SqsWorker.configuration)
-    end
 
     def find_queue(queue_name)
       sqs.queues.named(queue_name.to_s)
@@ -16,7 +12,9 @@ module SqsWorker
 
     private
 
-    attr_reader :sqs
+    def sqs
+      @sqs ||= ::AWS::SQS.new(SqsWorker.configuration)
+    end
 
   end
 end
