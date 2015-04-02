c6705388aa6f049a1f4995511267cf60b7e3919c
Jacob Evans <jacob@dekz.net>
2015-04-02 15:16:11 +1100

    Fix typo

--- lib/sqs_worker/error_handlers.rb
+++ lib/sqs_worker/error_handlers.rb
@@ -1,7 +1,7 @@
-SqsWorker::ErrorHandlerRegistry.register(Proc.new do |e|
+SqsWorker::ErrorHandlerRegistry.register(Proc.new do |exception|
   Airbrake.notify(exception)
 end) if defined?(Airbrake)
 
-SqsWorker::ErrorHandlerRegistry.register(Proc.new do |e|
+SqsWorker::ErrorHandlerRegistry.register(Proc.new do |exception|
   ActiveRecord::Base.clear_active_connections!
 end)if defined?(ActiveRecord)
