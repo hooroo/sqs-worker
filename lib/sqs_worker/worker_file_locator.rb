8de59fe7dc45e88f449d2234aeb127be367751c2
Brent Snook <brent@fuglylogic.com>
2015-06-03 21:57:12 +1000

    [hooroo/transact#78] Don’t explode when worker root doesn’t exist.

--- lib/sqs_worker/worker_file_locator.rb
+++ lib/sqs_worker/worker_file_locator.rb
@@ -2,7 +2,11 @@ module SqsWorker
   class WorkerFileLocator
 
     def self.locate
-      Dir.entries(SqsWorker.config.worker_root).select do |file_name|
+      root = SqsWorker.config.worker_root
+
+      return [] unless Dir.exists?(root)
+
+      Dir.entries(root).select do |file_name|
         file_name.end_with?('worker.rb')
       end.reverse
     end
--- spec/sqs_worker/worker_file_locator_spec.rb
+++ spec/sqs_worker/worker_file_locator_spec.rb
@@ -15,24 +15,38 @@ module SqsWorker
         allow(Dir).to receive(:entries).and_return(
           %w(first_test_worker.rb second_test_worker.rb invalid.rb)
         )
+        allow(Dir).to receive(:exists?).with('worker_root').and_return(directory_exists)
         allow(SqsWorker.config).to receive(:worker_root).and_return('worker_root')
       end
 
-      it 'looks for workers under the configured worker root' do
-        located_files
-        expect(Dir).to have_received(:entries).with('worker_root')
+      context "when the worker root isn't an existing directory" do
+        let(:directory_exists) { false }
+
+        it 'finds no files' do
+          expect(located_files).to be_empty
+        end
       end
 
-      it "only includes files that end with 'worker.rb'" do
-        expect(located_files).to include('first_test_worker.rb', 'second_test_worker.rb')
-      end
+      context "when the worker root is an existing directory" do
 
-      it "doesn't include files that don't end with 'worker.rb'" do
-        expect(located_files).not_to include("invalid.rb")
-      end
+        let(:directory_exists) { true }
 
-      it "reverses the order of files" do
-        expect(located_files).to eq(%w(second_test_worker.rb first_test_worker.rb))
+        it 'looks for workers under the configured worker root' do
+          located_files
+          expect(Dir).to have_received(:entries).with('worker_root')
+        end
+
+        it "only includes files that end with 'worker.rb'" do
+          expect(located_files).to include('first_test_worker.rb', 'second_test_worker.rb')
+        end
+
+        it "doesn't include files that don't end with 'worker.rb'" do
+          expect(located_files).not_to include("invalid.rb")
+        end
+
+        it "reverses the order of files" do
+          expect(located_files).to eq(%w(second_test_worker.rb first_test_worker.rb))
+        end
       end
     end
   end
