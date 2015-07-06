b6dc694a197bc04bb5e32df8153d1af44670830a
Chris Rode <chris@hooroo.com>
2015-07-06 14:12:01 +1000

    [hooroo/transact#25] Expanding the runtime dependency of the gem as it was causing conflicts with other gems. Also included a rake task for starting sqs workers

---
+++ lib/sqs_worker/rake.rb
@@ -0,0 +1,18 @@
+require 'sqs_worker'
+
+namespace :sqs_worker do
+
+  task :run_all => :environment do
+    loop do
+      begin
+        break unless AWS::SQS.new.queues.first.nil?
+      rescue Errno::ECONNREFUSED
+        puts "Waiting for the SQS Server to start..."
+        sleep(0.5)
+      end
+    end
+    puts "Starting the SqsWorkers"
+    SqsWorker.run_all
+  end
+
+end
