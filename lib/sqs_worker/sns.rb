b1c4a6c03fdbf9b0d46c64fcd0f626e00f1bc9fe
Brent Snook <brent@fuglylogic.com>
2015-08-03 13:46:45 +1000

    Revert "[hooroo/transact#136] Create SNS topic from listed topic details."
    This reverts commit 928f71c17cef25df477cebbf5b42331dd087d6b2.

--- lib/sqs_worker/sns.rb
+++ lib/sqs_worker/sns.rb
@@ -4,31 +4,27 @@ require 'sqs_worker/topic'
 require 'sqs_worker/errors'
 
 module SqsWorker
-  class Sns
+  class Sns < SimpleDelegator
 
     include Singleton
 
     def initialize
-      @sns_client = ::Aws::SNS::Client.new
+      @sns = ::Aws::SNS.new
+      super(@sns)
     end
 
     def find_topic(topic_name)
-      found_topic_names = []
-      sns_client.list_topics.each do |listed_topic|
-        found_topic_names << found_topic_name = listed_topic.topic_arn.split(':').last
-        return create_topic_from(listed_topic) if topic_name == found_topic_name
+      sns.topics.each do |topic|
+        return Topic.new(topic) if topic_name == topic.attributes['DisplayName']
       end
-      raise SqsWorker::Errors::NonExistentTopic, "No topic found with name '#{topic_name}', found these topics: #{found_topic_names.join(', ')}"
+      found_topic_names = sns.topics.map{ |topic| topic.attributes['DisplayName'] }.join(', ')
+      raise SqsWorker::Errors::NonExistentTopic, "No topic found with name '#{topic_name}', found these topics: #{found_topic_names}"
     end
 
     private
 
-    attr_reader :sns_client
+    attr_reader :sns
 
-    def create_topic_from(listed_topic)
-      topic = ::Aws::SNS::Topic.new(arn: listed_topic.topic_arn, client: sns_client)
-      return Topic.new(topic)
-    end
   end
 
 end
