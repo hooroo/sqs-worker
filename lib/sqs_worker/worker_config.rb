7450a14cabf29c4e088f2b07c25d9529fde4debf
Michael Chapman <michaelc@hooroo.com>
2017-01-13 16:01:15 +1100

    Verify that the queue exists when the SQS worker is configured

--- lib/sqs_worker/worker_config.rb
+++ lib/sqs_worker/worker_config.rb
@@ -9,7 +9,6 @@ module SqsWorker
     attr_reader :num_processors, :num_fetchers, :num_batchers, :num_deleters, :fetcher_batch_size, :queue_name, :empty_queue_throttle
 
     def initialize(config)
-
       raise 'You must specify a queue name for all SqsWorker classes.' unless config[:queue_name]
 
       num_processors = [config[:processors].to_i, MIN_PROCESSORS].max
@@ -22,6 +21,14 @@ module SqsWorker
       @empty_queue_throttle = config[:empty_queue_throttle] || DEFAULT_EMPTY_QUEUE_THROTTLE
       @fetcher_batch_size = [(@num_processors / @num_fetchers).to_i, MAX_FETCH_BATCH_SIZE].min
 
+      verify_queue_exists!
+    end
+
+
+    private
+
+    def verify_queue_exists!
+      Sqs.instance.find_queue(queue_name)
     end
 
   end
--- spec/sqs_worker/unit/worker_config_spec.rb
+++ spec/sqs_worker/unit/worker_config_spec.rb
@@ -6,134 +6,161 @@ module SqsWorker
 
     subject(:worker_config) { described_class.new(config) }
 
-    context 'with simple configuration' do
-
-      let(:config) do
-        {
-          queue_name: 'queue',
-          processors: 20,
-          empty_queue_throttle: 5
-        }
-      end
-
-      it 'sets queue name' do
-        expect(worker_config.queue_name).to eq config[:queue_name]
-      end
-
-      it 'sets empty queue throttle' do
-        expect(worker_config.empty_queue_throttle).to eq config[:empty_queue_throttle]
-      end
-
-      it 'sets the number of processors' do
-        expect(worker_config.num_processors).to eq config[:processors]
-      end
-
-      it 'sets the fetcher batch size to the correct size' do
-        expect(worker_config.fetcher_batch_size).to eq(config[:processors] / worker_config.num_fetchers)
-      end
-
-      it 'sets the number of fetchers to the correct size' do
-        expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of batchers to the optimal size' do
-        expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of deleters to the optimal size' do
-        expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
+    let(:config) do
+      {
+        queue_name: queue_name,
+        processors: 20,
+        empty_queue_throttle: 5
+      }
     end
 
-    context 'with number of processors less than minimum' do
+    let(:queue_name) { 'queue-me-up' }
+    let(:logger) { double('logger', debug: nil) }
 
-      let(:config) do
-        {
-          queue_name: 'queue',
-          empty_queue_throttle: 5,
-          processors: WorkerConfig::MIN_PROCESSORS - 1
-        }
-      end
-
-      it 'sets the number of processors to the minimum' do
-        expect(worker_config.num_processors).to eq WorkerConfig::MIN_PROCESSORS
-      end
-
-      it 'sets the number of fetchers to the minimum pool size' do
-        expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of batchers to the minimum pool size' do
-        expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of deleters to the minimum pool size' do
-        expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
+    before do
+      SqsWorker.logger = logger
     end
 
-    context 'with number of processors that maxes out the fetch batch size' do
+    context 'when the queue exists' do
 
-      let(:num_processors) { WorkerConfig::MAX_FETCH_BATCH_SIZE * WorkerConfig::MIN_POOL_SIZE + 1 }
-
-      let(:config) do
-        {
-          queue_name: 'queue',
-          empty_queue_throttle: 5,
-          processors: num_processors
-        }
+      before do
+        allow(Sqs.instance).to receive(:find_queue).with(queue_name)
       end
 
-      it 'sets the number of processors to the configured amount' do
-        expect(worker_config.num_processors).to eq num_processors
+      context 'with simple configuration' do
+
+        it 'sets queue name' do
+          expect(worker_config.queue_name).to eq config[:queue_name]
+        end
+
+        it 'sets empty queue throttle' do
+          expect(worker_config.empty_queue_throttle).to eq config[:empty_queue_throttle]
+        end
+
+        it 'sets the number of processors' do
+          expect(worker_config.num_processors).to eq config[:processors]
+        end
+
+        it 'sets the fetcher batch size to the correct size' do
+          expect(worker_config.fetcher_batch_size).to eq(config[:processors] / worker_config.num_fetchers)
+        end
+
+        it 'sets the number of fetchers to the correct size' do
+          expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of batchers to the optimal size' do
+          expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of deleters to the optimal size' do
+          expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
       end
 
-      it 'sets the fetcher batch size to the maximum allowable size (defined by aws-sdk)' do
-        expect(worker_config.fetcher_batch_size).to eq(WorkerConfig::MAX_FETCH_BATCH_SIZE)
+      context 'with number of processors less than minimum' do
+
+        let(:config) do
+          {
+            queue_name: queue_name,
+            empty_queue_throttle: 5,
+            processors: WorkerConfig::MIN_PROCESSORS - 1
+          }
+        end
+
+        it 'sets the number of processors to the minimum' do
+          expect(worker_config.num_processors).to eq WorkerConfig::MIN_PROCESSORS
+        end
+
+        it 'sets the number of fetchers to the minimum pool size' do
+          expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of batchers to the minimum pool size' do
+          expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of deleters to the minimum pool size' do
+          expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
       end
 
+      context 'with number of processors that maxes out the fetch batch size' do
+
+        let(:num_processors) { WorkerConfig::MAX_FETCH_BATCH_SIZE * WorkerConfig::MIN_POOL_SIZE + 1 }
+
+        let(:config) do
+          {
+            queue_name: queue_name,
+            empty_queue_throttle: 5,
+            processors: num_processors
+          }
+        end
+
+        it 'sets the number of processors to the configured amount' do
+          expect(worker_config.num_processors).to eq num_processors
+        end
+
+        it 'sets the fetcher batch size to the maximum allowable size (defined by aws-sdk)' do
+          expect(worker_config.fetcher_batch_size).to eq(WorkerConfig::MAX_FETCH_BATCH_SIZE)
+        end
+
+      end
+
+      context 'with missing and defaultable configuration' do
+
+        let(:config) do
+          {
+            queue_name: queue_name
+          }
+        end
+
+        it 'sets the number of processors to the default' do
+          expect(worker_config.num_processors).to eq WorkerConfig::MIN_PROCESSORS
+        end
+
+        it 'sets the number of fetchers to the minimum pool size' do
+          expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of batchers to the minimum pool size' do
+          expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the number of deleters to the minimum pool size' do
+          expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
+        end
+
+        it 'sets the emoty_queue_throttle to the default' do
+          expect(worker_config.empty_queue_throttle).to eq(WorkerConfig::DEFAULT_EMPTY_QUEUE_THROTTLE)
+        end
+
+      end
+
+      context 'without a queue name' do
+
+        let(:config) { {} }
+
+        it 'raises error' do
+          expect{ worker_config }.to raise_error
+        end
+      end
     end
 
-    context 'with missing and defaultable configuration' do
+    context 'when the queue does not exist' do
 
-      let(:config) do
-        {
-          queue_name: 'queue'
-        }
+      # RSpec throws an error for some reason because the original logger has already been used
+      let(:second_logger) { double('logger', debug: nil) }
+
+      before do
+        SqsWorker.logger = second_logger
       end
 
-      it 'sets the number of processors to the default' do
-        expect(worker_config.num_processors).to eq WorkerConfig::MIN_PROCESSORS
+      it 'raises an error' do
+        expect { worker_config }.to raise_error(SqsWorker::Errors::NonExistentQueue)
       end
-
-      it 'sets the number of fetchers to the minimum pool size' do
-        expect(worker_config.num_fetchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of batchers to the minimum pool size' do
-        expect(worker_config.num_batchers).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the number of deleters to the minimum pool size' do
-        expect(worker_config.num_deleters).to eq(WorkerConfig::MIN_POOL_SIZE)
-      end
-
-      it 'sets the emoty_queue_throttle to the default' do
-        expect(worker_config.empty_queue_throttle).to eq(WorkerConfig::DEFAULT_EMPTY_QUEUE_THROTTLE)
-      end
-
-    end
-
-    context 'without a queue name' do
-
-      let(:config) { {} }
-
-      it 'raises error' do
-        expect{ worker_config }.to raise_error
-      end
-
     end
 
   end
