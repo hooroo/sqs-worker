6450890a6a1548fd3c56e58f7b50dd3843b04d28
Kamal Rizk <kimomicho@hotmail.com>
2020-03-16 12:53:06 +1100

    Revert "switch aws logger to "info" instead of "debugger""
    This reverts commit 3a5c87293bf040d0cd1b13ae44350ff1507f94af.

--- lib/sqs_worker/sqs.rb
+++ lib/sqs_worker/sqs.rb
@@ -9,8 +9,7 @@ module SqsWorker
     include Singleton
 
     def initialize
-      Aws.config.update({ log_level: :info })
-      # debug log level was logging sqs messages which caused PI leakage for flightbooking events
+      Aws.config.update({ log_level: :debug })
       @sqs = ::Aws::SQS::Resource.new(logger: SqsWorker.logger)
       @queue_cache = {}
       super(@sqs)
